d60954e99a3ee6b108f0596bb446b9bfaad5dc60
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-08-06 11:48:29 -0300

    06/08/2022

---
+++ Trabajo Práctico Número 1 - Algoritmos y DF/Programas/Ejercicio 02/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_02
+{
+    public partial class Form1 : Form
+    {
+        char[,] matriz;
+        int i, j;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            matriz = new char[5, 5];
+            for (i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = ' ';
+                }
+            }
+            i = 0;
+            for (j = 0; j < matriz.GetLength(0); j++)
+            {
+                matriz[i, j] = '*';
+                if(j<2)
+                {
+                    i++;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            string resultado = "";
+            for (i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (j = 0; j < matriz.GetLength(1); j++)
+                {
+                    resultado += matriz[i, j];
+                    if (j == matriz.GetLength(1) - 1)
+                    {
+                        resultado += Environment.NewLine;
+                    }
+                }
+            }
+            textBox1.Text = resultado;
+        }
+    }
+}
