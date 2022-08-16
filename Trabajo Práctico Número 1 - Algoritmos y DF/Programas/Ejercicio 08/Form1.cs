59ebb82f850d400b3d749e6e8296b29994041bff
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-08-16 10:09:35 -0300

    16/08/2022

---
+++ Trabajo Práctico Número 1 - Algoritmos y DF/Programas/Ejercicio 08/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_08
+{
+    public partial class Form1 : Form
+    {
+        int n, a, b, c, x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "";
+                n = int.Parse(textBox1.Text);
+                a = 0;
+                b = 0;
+                c = 0;
+                x= 0;
+                label1.Text += $"{c}";
+                c = 1;
+                x++;
+                while (x < n)
+                {
+                    label1.Text += $"-{c}";
+                    a = b;
+                    b = c;
+                    c = a + b;
+                    x++;
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
