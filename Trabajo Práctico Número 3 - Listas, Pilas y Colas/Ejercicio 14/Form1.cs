584a8633806fb52dee50b4f80d510faaedcb3b41
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-09-04 17:43:04 -0300

    04/09/2022

---
+++ Trabajo Práctico Número 3 - Listas, Pilas y Colas/Ejercicio 14/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_14
+{
+    public partial class Form1 : Form
+    {
+        Pila pila1, pila2, pila3;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pila1 = new Pila();
+            pila2 = new Pila();
+            pila3 = new Pila();
+            Random ran = new Random();
+            for(int i=0; i<30; i++)
+            {
+                pila1.Apilar(ran.Next(1, 21));
+            }
+            Pila aux = new Pila();
+            while(pila1.Ver() != null)
+            {
+                aux.Apilar(pila1.Desapilar());
+            }
+            while(aux.Ver()!=null)
+            {
+                Nodo nodo = aux.Desapilar();
+                pila1.Apilar(nodo.Id);
+                if (nodo.Id % 2 == 0) pila2.Apilar(nodo.Id);
+                else pila3.Apilar(nodo.Id);
+            }
+            Mostrar(pila1, listBox1);
+            Mostrar(pila2, listBox2);
+            Mostrar(pila3, listBox3);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Mostrar(Pila pila, ListBox listBox)
+        {
+            Pila aux = new Pila();
+            listBox.Items.Clear();
+            while (pila.Ver() != null)
+            {
+                aux.Apilar(pila.Desapilar());
+                listBox.Items.Add(aux.Ver().Id.ToString());
+            }
+            while (aux.Ver() != null)
+            {
+                pila.Apilar(aux.Desapilar());
+            }
+        }
+    }
+}
---
+++ Trabajo Práctico Número 3 - Listas, Pilas y Colas/Ejercicio 14/Pila.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+    public class Pila
+    {
+        Nodo centinela;
+        public Pila()
+        {
+            centinela = new Nodo();
+        }
+        public void Apilar(int id)
+        {
+            Nodo aux = new Nodo(id);
+            if (centinela.Siguiente == null)
+            {
+                centinela.Siguiente = aux;
+            }
+            else
+            {
+                aux.Siguiente = centinela.Siguiente;
+                centinela.Siguiente = aux;
+            }
+        }
+        public void Apilar(Nodo nodo)
+        {
+            Nodo aux = nodo;
+            if (centinela.Siguiente == null)
+            {
+                centinela.Siguiente = aux;
+            }
+            else
+            {
+                aux.Siguiente = centinela.Siguiente;
+                centinela.Siguiente = aux;
+            }
+        }
+        public Nodo Desapilar()
+        {
+            Nodo nodoRta = centinela.Siguiente;
+            if (centinela.Siguiente != null)
+            {
+                Nodo aux = nodoRta.Siguiente;
+                nodoRta.Siguiente = null;
+                centinela.Siguiente = aux;
+            }
+            return nodoRta;
+        }
+        public Nodo Ver()
+        {
+            Nodo nodoRta = null;
+            if (centinela.Siguiente != null)
+            {
+                nodoRta = new Nodo(centinela.Siguiente.Id);
+            }
+            return nodoRta;
+        }
+    }
+    public class Nodo
+    {
+        public Nodo(int id = 0) { Id = id; Siguiente = null; }
+        public int Id { get; }
+        public Nodo Siguiente { get; set; }
+    }
+}
